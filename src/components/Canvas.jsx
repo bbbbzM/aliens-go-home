7d23ccae98ffe821fb245d535d1c299622c3e3de
Libinzi Ma <504321494@qq.com>
2019-12-28 20:18:47 +0800

    添加炮弹，飞碟，生命

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,23 +4,33 @@ import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
+import CannonBall from './CannonBall';
+import CurrentScore from './CurrentScore';
+import FlyingObject from './FlyingObject';
+import Heart from './Heart';
 
 const Canvas = (props) => {
     const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
-    const style = {
-        border: '1px solid black',
-    }
     return (
         <svg
             id="aliens-go-home-canvas"
             viewBox={viewBox}
-            preserveAspectRatio="xMaxYMax"
             onMouseMove={props.trackMouse}
         >
+            <defs>
+                <filter id="shadow">
+                    <feDropShadow dx="1" dy="2" stdDeviation="2" />
+                </filter>
+            </defs>
             <Sky />
             <Ground />
             <CannonBase />
             <CannonPipe rotation={props.angle} />
+            <CannonBall position={{ x: 0, y: -100 }} />
+            <CurrentScore score={15} />
+            <FlyingObject position={{ x: -150, y: -300 }} />
+            <FlyingObject position={{ x: 150, y: -300 }} />
+            <Heart position={{ x: -300, y: 35 }} />
         </svg>
     )
 }
