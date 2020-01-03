eb77750f13c4e495d1fa3a75d0663ef7ac4693a6
Libinzi Ma <504321494@qq.com>
2020-01-03 15:25:48 +0800

    碰撞检测

--- src/reducers/moveObjects.js
+++ src/reducers/moveObjects.js
@@ -1,18 +1,26 @@
 import { calculateAngle } from '../utils/formulas';
 import createFlyingObjects from './createFlyingObjects';
 import moveBalls from './moveCannonBalls';
+import checkCollisions from './checkCollisions';
 
 function moveObjects(state, action) {
     if (!state.gameState.started) return state;
     const mousePosition = action.mousePosition || { x: 0, y: 0 }
     const newState = createFlyingObjects(state);
     const now = (new Date()).getTime();
-    const flyingObjects = newState.gameState.flyingObjects.filter((flyingObject) => {
+    let flyingObjects = newState.gameState.flyingObjects.filter((flyingObject) => {
         return ((now - flyingObject.createdAt) < 4000);
     });
     const { x, y } = mousePosition;
     const angle = calculateAngle(0, 0, x, y);
-    const cannonBalls = moveBalls(state.gameState.cannonBalls);
+    let cannonBalls = moveBalls(state.gameState.cannonBalls);
+    const objectsDestroyed = checkCollisions(cannonBalls, flyingObjects);
+    const cannonBallsDestroyed = objectsDestroyed.map(object => (object.cannonBallId));
+    const flyingDiscsDestroyed = objectsDestroyed.map(object => (object.flyingDiscId));
+
+    cannonBalls = cannonBalls.filter(cannonBall => (cannonBallsDestroyed.indexOf(cannonBall.id)));
+    flyingObjects = flyingObjects.filter(flyingDisc => (flyingDiscsDestroyed.indexOf(flyingDisc.id)));
+
     return {
         ...newState,
         gameState: {
